2807d60a8bd010a685f5fcb6861ca72d41f0364a
MeeXaSiK <87555873+MeeXaSiK@users.noreply.github.com>
2021-10-14 11:03:44 +0700

    Update NightCache.cs

--- Code/NightCache/NightCache.cs
+++ Code/NightCache/NightCache.cs
@@ -5,17 +5,14 @@ namespace NTC.Global.Cache
     [RequireComponent(typeof(NightCacheInstallMachine))]
     public abstract class NightCache : MonoBehaviour, INightCached
     {
-        public GameObject CachedGameObject =>
-            cachedGameObject ? cachedGameObject : cachedGameObject = gameObject;
-        
-        public Transform CachedTransform =>
-            cachedTransform ? cachedTransform : cachedTransform = transform;
-
         private GameObject cachedGameObject;
         private Transform cachedTransform;
         
         private bool systemIsActiveInScene;
-        
+
+        public GameObject CachedGameObject => cachedGameObject ??= gameObject;
+        public Transform CachedTransform => cachedTransform ??= transform;
+
         public bool IsActive()
         {
             return systemIsActiveInScene;
